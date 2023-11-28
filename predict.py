5472a3e01e0501555c2b78f15b15d804b3e33c8a
Mujib <mrc.firoj@gmail.com>
2023-11-28 08:29:01 -0800

    predict file added

---
+++ predict.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Mujib
+
+Prediction
+
+Predict using this line of code: ignore the error. 
+"""
+
+#======  ===========
+
+
+!yolo task = segment mode = predict model = 'Results_202311/From_colab_20231125_train5-20231125T181127Z-001/train5/weights/best.pt' source = 'C:/All_data_from_Box/Miscellaneous_copy/Data_Projects/Computer_Vision_And_DL/Yolo_V8/images_wood120/data_final/images/test'
--- train.py
+++ train.py
@@ -11,7 +11,3 @@ model = YOLO('yolov8n-seg.pt')
 
 model.train(data='config.yaml', epochs=100, imgsz=640)
 
-#====== Prediction ===========
-
-# Predict using this line of code: ignore the error. 
-!yolo task = segment mode = predict model = 'Results_202311/From_colab_20231125_train5-20231125T181127Z-001/train5/weights/best.pt' source = 'C:/All_data_from_Box/Miscellaneous_copy/Data_Projects/Computer_Vision_And_DL/Yolo_V8/images_wood120/data_final/images/test'
